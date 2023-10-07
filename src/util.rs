1185a62530d906063db1f63a73ca291ee8d1a6a2
Cameron Irvine <cameron@cameronirvine.com>
2023-10-08 02:03:57 +1100

    fix fs stuff on mac

--- src/util.rs
+++ src/util.rs
@@ -2,11 +2,10 @@ use std::fs;
 use std::process::exit;
 
 pub fn read_file_to_string(filename: String) -> Vec<String> {
-    let filename = format!(
-        "{}\\inputs\\{filename}",
-        std::env::current_dir().unwrap().display()
-    );
-    let read_res = &fs::read_to_string(filename);
+    let mut path = std::env::current_dir().unwrap();
+    path.push("inputs");
+    path.push(filename);
+    let read_res = &fs::read_to_string(path);
     match read_res {
         Ok(data) => {
             let lines: Vec<String> = data.lines().map(String::from).collect();
